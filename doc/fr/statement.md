b530c9cb12be61bb5bbd3ea95b1fc9b1b54bf782
padget <padget.pro@gmail.com>
2019-03-21 13:09:25 +0100

    Creation of CMakeList

---
+++ doc/fr/statement.md
@@ -0,0 +1,13 @@
+# Instructions
+
+Les instructions sont la colonne vertebrale de blocks.
+La premiere d'entre elle est la declaration d'un bloc.
+Elle se fait en apposant un nom et le symbol ':'.
+
+Exemple : 
+  main: # declaration du bloc main (point d'entree du programme).
+  ...
+
+Une instruction peut avoir 0, 1 ou deux operande.
+Une instruction est sur une seule ligne.
+Chaque ligne est une instruction differente.
---
+++ src/blocks.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <array>
+#include <memory>
+#include <type_traits>
+#include <fstream>
+
+#include <block.hpp>
+
+constexpr int add(int l, int r)
+{
+	return l + r;
+}
+
+constexpr int minus(int l, int r)
+{
+	return l - r;
+}
+
+constexpr int divide(int l, int r)
+{
+	return l / r;
+}
+
+constexpr int mult(int l, int r)
+{
+	return l * r;
+}
+
+int main()
+{
+	block<50> memory;
+	block<50> funcs;
+
+	block_manager<block<50>> func_manager(funcs);
+	auto iter = func_manager.store(&add);
+	auto minus = func_manager.read<decltype(&add)>(iter);
+
+	block_manager<block<50>> data_manager(funcs);
+
+	auto iter12 = data_manager.store(12);
+	auto iter4 = data_manager.store(4);
+
+	auto res = minus(
+			data_manager.read<int>(iter12),
+			data_manager.read<int>(iter4));
+
+	auto iterres = data_manager.store(res);
+
+	std::cout << data_manager.read<int>(iterres) << std::endl;
+	std::cout << sizeof(std::fstream) << std::endl;
+}
