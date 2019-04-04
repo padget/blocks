ebddd3dadc6e3913f3e8c7595cecae6bf7b47e40
padget <padget.pro@gmail.com>
2019-04-04 17:14:51 +0200

    add error management for lexical scan

--- include/bml.hpp
+++ include/bml.hpp
@@ -12,19 +12,19 @@
 
 namespace mystd
 {
-    template<class InputIt,
-             class UnaryPredicate>
-    constexpr InputIt find_if (InputIt first, InputIt last, UnaryPredicate p)
+  template<class InputIt,
+           class UnaryPredicate>
+  constexpr InputIt find_if (InputIt first, InputIt last, UnaryPredicate p)
+  {
+    for (; first!=last; ++first)
     {
-        for (; first!=last; ++first)
-        {
-            if (p(*first))
-            {
-                return first;
-            }
-        }
-        return last;
+      if (p(*first))
+      {
+        return first;
+      }
     }
+    return last;
+  }
 }
 
 
@@ -33,9 +33,9 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_colon (iterator begin,
               iterator end)
 {
-    return (begin!=end&&*begin==':') ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
+  return (begin!=end&&*begin==':') ?
+         std::optional(std::pair(std::next(begin), type)) :
+         std::nullopt;
 }
 
 
@@ -44,9 +44,9 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_indent (iterator begin,
                iterator end)
 {
-    return (begin!=end&&*begin=='\t') ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
+  return (begin!=end&&*begin=='\t') ?
+         std::optional(std::pair(std::next(begin), type)) :
+         std::nullopt;
 }
 
 
@@ -55,9 +55,9 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_space (iterator begin,
               iterator end)
 {
-    return (begin!=end&&*begin==' ') ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
+  return (begin!=end&&*begin==' ') ?
+         std::optional(std::pair(std::next(begin), type)) :
+         std::nullopt;
 }
 
 
@@ -66,20 +66,9 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_eol (iterator begin,
             iterator end)
 {
-    return (begin!=end&&*begin=='\n') ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
-}
-
-
-template<typename iterator, auto type>
-std::optional<std::pair<iterator, decltype(type)>>
-detect_eof (iterator begin,
-            iterator end)
-{
-    return (begin==end) ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
+  return (begin!=end&&*begin=='\n') ?
+         std::optional(std::pair(std::next(begin), type)) :
+         std::nullopt;
 }
 
 
@@ -88,18 +77,18 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_label (iterator begin,
               iterator end)
 {
-    auto cursor = begin;
+  auto cursor = begin;
 
-    while (cursor!=end&&
-           'a'<=*cursor&&
-           *cursor<='z')
-    {
-        ++cursor;
-    }
+  while (cursor!=end&&(
+    ('a'<=*cursor&&*cursor<='z')||
+    *cursor=='$'))
+  {
+    ++cursor;
+  }
 
-    return (cursor!=begin) ?
-           std::optional(std::pair(cursor, type)) :
-           std::nullopt;
+  return (cursor!=begin) ?
+         std::optional(std::pair(cursor, type)) :
+         std::nullopt;
 }
 
 
@@ -108,9 +97,9 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_tabitem (iterator begin,
                 iterator end)
 {
-    return (begin!=end&&*begin=='[') ?
-           std::optional(std::pair(std::next(begin), type)) :
-           std::nullopt;
+  return (begin!=end&&*begin=='[') ?
+         std::optional(std::pair(std::next(begin), type)) :
+         std::nullopt;
 }
 
 
@@ -119,18 +108,18 @@ std::optional<std::pair<iterator, decltype(type)>>
 detect_number (iterator begin,
                iterator end)
 {
-    auto cursor = begin;
+  auto cursor = begin;
 
-    while (cursor!=end&&
-           '0'<=*cursor&&
-           *cursor<='9')
-    {
-        ++cursor;
-    }
+  while (cursor!=end&&
+         '0'<=*cursor&&
+         *cursor<='9')
+  {
+    ++cursor;
+  }
 
-    return (cursor!=begin) ?
-           std::optional(std::pair(cursor, type)) :
-           std::nullopt;
+  return (cursor!=begin) ?
+         std::optional(std::pair(cursor, type)) :
+         std::nullopt;
 }
 
 
@@ -141,23 +130,24 @@ auto detect (iterator begin, iterator end,
              detector d, detectors ... ds)
 -> decltype(d(begin, end))
 {
-    auto results = std::array{d(begin, end), ds(begin, end)...};
-    auto has_value = [] (auto const &opt)
-    { return opt.has_value(); };
-    auto found = mystd::find_if(std::begin(results),
-                                std::end(results),
-                                has_value);
+  auto results = std::array{d(begin, end), ds(begin, end)...};
+  auto has_value = [] (auto const &opt)
+  { return opt.has_value(); };
+  auto found = mystd::find_if(std::begin(results),
+                              std::end(results),
+                              has_value);
 
-    if (found!=std::end(results))
-        return *found;
-    else
-        return std::nullopt;
+  if (found!=std::end(results))
+    return *found;
+  else
+    return std::nullopt;
 }
 
 
 enum class bml : int
 {
-    unknown, label, indent, space, number, colon, eol, eof, tabitem, string /* TODO faire la fonction pour string */
+  unknown, label, indent, space, number, colon, eol, tabitem, string /* TODO faire la fonction pour string */
+  // 0   , 1    , 2     , 3    , 4     , 5    , 6  , 7      , 8
 };
 
 
@@ -165,78 +155,83 @@ template<typename str_t = std::string>
 class lexer
 {
 private:
-    using iterator = typename str_t::const_iterator;
+  using iterator = typename str_t::const_iterator;
 
-    iterator __cursor;
-    iterator __end;
-
-public:
-    explicit lexer (const str_t &input)
-        : __cursor(std::begin(input)),
-          __end(std::end(input))
-    {}
-
-
-    struct token
-    {
-        bml type;
-        str_t value;
-    };
-
+  iterator __cursor;
+  iterator __end;
+  unsigned __line = 1;
 
 public:
-    token next ()
-    {
-        auto res = detect(
-            __cursor, __end,
-            &detect_tabitem<iterator, bml::tabitem>,
-            &detect_colon<iterator, bml::colon>,
-            &detect_eof<iterator, bml::eof>,
-            &detect_eol<iterator, bml::eol>,
-            &detect_number<iterator, bml::number>,
-            &detect_label<iterator, bml::label>,
-            &detect_indent<iterator, bml::indent>,
-            &detect_space<iterator, bml::space>);
-
-        bool valued = res.has_value();
-
-        auto &&type = valued ? std::get<1>(res.value()) : bml::unknown;
-        auto &&iter = valued ? std::get<0>(res.value()) : std::next(__cursor);
-        auto &&value = str_t(__cursor, iter);
-
-        auto tk = token{type, value};
-        __cursor = valued ? std::get<0>(res.value()) : std::next(__cursor);
-        return tk;
-    }
+  explicit lexer (const str_t &input)
+    : __cursor(std::begin(input)),
+      __end(std::end(input))
+  {}
 
 
-    bool has_next () const
-    {
-        return __cursor not_eq __end;
-    }
+  struct token
+  {
+    bml type;
+    str_t value;
+    unsigned line;
+  };
 
 
-    std::vector<token> tokens ()
-    {
-        std::list<token> tks;
+public:
+  token next ()
+  {
+    auto res = detect(
+      __cursor, __end,
+      &detect_tabitem<iterator, bml::tabitem>,
+      &detect_colon<iterator, bml::colon>,
+      &detect_eol<iterator, bml::eol>,
+      &detect_number<iterator, bml::number>,
+      &detect_label<iterator, bml::label>,
+      &detect_indent<iterator, bml::indent>,
+      &detect_space<iterator, bml::space>);
 
-        while (has_next())
-            tks.push_back(next());
+    bool valued = res.has_value();
 
-        auto begin = std::begin(tks);
-        auto end = std::end(tks);
+    auto &&type = valued ? std::get<1>(res.value()) : bml::unknown;
+    auto &&iter = valued ? std::get<0>(res.value()) : std::next(__cursor);
+    auto &&value = str_t(__cursor, iter);
 
-        std::list<token> tks2;
-        auto end2 = std::remove_if(
-            std::begin(tks), std::end(tks),
-            [] (const auto &tk)
-            { return tk.type==bml::space; });
+    auto tk = token{type, value, __line};
 
-        std::vector<token> res(
-            std::make_move_iterator(std::begin(tks)),
-            std::make_move_iterator(end2));
-        return res;
-    }
+    if (type==bml::eol)
+      __line++;
+
+    __cursor = valued ? std::get<0>(res.value()) : std::next(__cursor);
+    return tk;
+  }
+
+
+  bool has_next () const
+  {
+    return __cursor not_eq __end;
+  }
+
+
+  std::vector<token> tokens ()
+  {
+    std::list<token> tks;
+
+    while (has_next())
+      tks.push_back(next());
+
+    auto begin = std::begin(tks);
+    auto end = std::end(tks);
+
+    std::list<token> tks2;
+    auto end2 = std::remove_if(
+      std::begin(tks), std::end(tks),
+      [] (const auto &tk)
+      { return tk.type==bml::space; });
+
+    std::vector<token> res(
+      std::make_move_iterator(std::begin(tks)),
+      std::make_move_iterator(end2));
+    return res;
+  }
 };
 
 
@@ -244,133 +239,201 @@ template<typename iterator>
 bool is_not_end (const iterator &begin,
                  const iterator &end)
 {
-    return begin!=end;
+  return begin!=end;
 }
 
 
 template<typename iterator>
 bool is_not_eol (const iterator &begin)
 {
-    return (*begin).type!=bml::eol;
+  return (*begin).type!=bml::eol;
 }
 
 
 template<typename iterator>
 bool is_indent (const iterator &begin)
 {
-    return (*begin).type==bml::indent;
+  return (*begin).type==bml::indent;
 }
 
 
 template<typename iterator>
 bool is_arg (const iterator &begin)
 {
-    const auto &type = (*begin).type;
-    return type==bml::label||
-           type==bml::number||
-           type==bml::tabitem||
-           type==bml::string;
+  const auto &type = (*begin).type;
+
+  switch (type)
+  {
+    case bml::label:
+    case bml::number:
+    case bml::tabitem:
+    case bml::string:
+      return true;
+    default:
+      return false;
+  }
 }
 
 
 template<typename iterator>
 bool is_command_name (const iterator &begin)
 {
-    return (*begin).type==bml::label;
+  return (*begin).type==bml::label;
+}
+
+
+template<typename iterator>
+bool is_colon (const iterator &begin)
+{
+  return (*begin).type==bml::colon;
 }
 
 
 struct command
 {
-    unsigned context;
-    std::string name;
-    std::vector<std::string> args;
+  unsigned context;
+  std::string name;
+  std::vector<std::string> args;
 };
 
 
 template<typename iterator>
-std::optional<command>
-parse_line (iterator begin, iterator end)
+bool has_lexical_error (const iterator &begin,
+                        const iterator &end)
 {
-    if (is_not_end(begin, end)&&
-        is_not_eol(begin))
+  return std::find_if(begin, end, [] (auto &&tk)
+  { return tk.type==bml::unknown; })!=end;
+}
+
+
+template<typename iterator>
+void print_lexical_error (iterator begin)
+{
+  const auto &tk = *begin;
+  if (tk.type==bml::unknown)
+  {
+    std::cerr<<"unexpected character "<<tk.value<<" at l."<<tk.line<<'\n';
+  }
+}
+
+
+template<typename iterator>
+void print_lexical_errors (iterator begin,
+                           iterator end)
+{
+  while (is_not_end(begin, end))
+  {
+    print_lexical_error(begin);
+    begin++;
+  }
+}
+
+template <typename char_t>
+command command_in_error (const std::basic_string<char_t>& why)
+{
+  return command{ // todo faire une meilleure gestion des erreurs de syntaxe...
+    .name=why
+  };
+}
+
+template<typename iterator>
+std::optional<command>
+parse_line (iterator begin,
+            iterator end)
+{
+  if (is_not_end(begin, end)&&
+      is_not_eol(begin))
+  {
+    if (has_lexical_error(begin, end))
     {
-        unsigned context = 0;
-        while (is_not_end(begin, end)&&
-               is_indent(begin))
-        {
-            begin++;
-            context++;
-        }
-
-        std::string name;
-
-        if (is_not_end(begin, end)&&
-            is_command_name(begin))
-        {
-            name = (*begin).value;
-            begin++;
-            begin++;
-        }
-
-        std::vector<std::string> args;
-
-        while (is_not_end(begin, end)&&
-               is_arg(begin))
-        {
-            args.push_back((*begin).value);
-            begin++;
-        }
-
-        return {command{
-            .context=context,
-            .name=name,
-            .args=args
-        }};
+      print_lexical_errors(begin, end);
+      return std::nullopt;
     }
     else
     {
+      unsigned context = 0;
+
+      while (is_not_end(begin, end)&&
+             is_indent(begin))
+      {
+        begin++;
+        context++;
+      }
+
+      std::string name;
+
+      if (is_not_end(begin, end)&&
+          is_command_name(begin)&&
+          is_not_end(std::next(begin), end)&&
+          is_colon(std::next(begin)))
+      {
+        name = (*begin).value;
+
+        begin++;
+        begin++;
+      }
+      else
+      {
         return std::nullopt;
+      }
+
+      std::vector<std::string> args;
+
+      while (is_not_end(begin, end)&&
+             is_arg(begin))
+      {
+        args.push_back((*begin).value);
+        begin++;
+      }
+
+      return command{
+        .context=context,
+        .name=name,
+        .args=args};
     }
+  }
+  else
+  {
+    return std::nullopt;
+  }
 }
 
 
-command command_in_error ()
-{
-    return command{ // todo faire une meilleure gestion des erreurs de syntaxe...
-        .name="command in error"
-    };
-}
+
 
 
 template<typename str_t=std::string>
 std::vector<command>
 parse (lexer<str_t> &lex)
 {
-    auto &&tokens = lex.tokens();
-    auto begin = std::begin(tokens), eol = begin;
-    auto end = std::end(tokens);
-    std::vector<command> commands;
-    const command error = command_in_error();
+  auto &&tokens = lex.tokens();
+  auto begin = std::begin(tokens), eol = begin;
+  auto end = std::end(tokens);
+  std::vector<command> commands;
+  const command error = command_in_error();
 
-    while (is_not_end(begin, end))
+  while (is_not_end(begin, end))
+  {
+    while (is_not_end(eol, end)&&
+           is_not_eol(eol))
     {
-        while (is_not_end(eol, end)&&
-               is_not_eol(eol))
-        {
-            eol++;
-        }
-
-        // between begin and eol we have a line
-        auto &&cmd = parse_line(begin, eol);
-
-        commands.push_back(cmd.value_or(error));
-        if (is_not_end(eol, end))
-            eol++;
-        begin = eol;
+      ++eol;
     }
 
-    return commands;
+    // between begin and eol we have a line
+    auto &&cmd = parse_line(begin, eol);
+
+    commands.push_back(cmd.value_or(error));
+
+    if (is_not_end(eol, end))
+    {
+      ++eol;
+    }
+
+    begin = eol;
+  }
+
+  return commands;
 }
 
 
--- src/blocks.cpp
+++ src/blocks.cpp
@@ -10,59 +10,87 @@
 
 constexpr int add (int l, int r)
 {
-    return l+r;
+  return l+r;
 }
 
 
 constexpr int minus (int l, int r)
 {
-    return l-r;
+  return l-r;
 }
 
 
 constexpr int divide (int l, int r)
 {
-    return l/r;
+  return l/r;
 }
 
 
 constexpr int mult (int l, int r)
 {
-    return l*r;
+  return l*r;
 }
 
 
 int main ()
 {
-    block<50> memory;
-    block<50> funcs;
+  block<50> memory;
+  block<50> funcs;
 
-    block_manager<block<50>> func_manager(funcs);
-    auto iter = func_manager.store(&add);
-    auto minus = func_manager.read<decltype(&add)>(iter);
+  block_manager<block<50>> func_manager(funcs);
+  auto iter = func_manager.store(&add);
+  auto minus = func_manager.read<decltype(&add)>(iter);
 
-    block_manager<block<50>> data_manager(funcs);
+  block_manager<block<50>> data_manager(funcs);
 
-    auto iter12 = data_manager.store(12);
-    auto iter4 = data_manager.store(4);
+  auto iter12 = data_manager.store(12);
+  auto iter4 = data_manager.store(4);
 
-    auto res = minus(
-        data_manager.read<int>(iter12),
-        data_manager.read<int>(iter4));
+  auto res = minus(
+    data_manager.read<int>(iter12),
+    data_manager.read<int>(iter4));
 
-    auto iterres = data_manager.store(res);
+  auto iterres = data_manager.store(res);
 
-    std::cout<<data_manager.read<int>(iterres)<<std::endl;
-    std::cout<<sizeof(std::fstream)<<std::endl;
+  std::cout<<data_manager.read<int>(iterres)<<std::endl;
+  std::cout<<sizeof(std::fstream)<<std::endl;
 
-    std::string bin = "printi: i\n"
-                      "\tlet: continue true";
+  std::string bin =
+    "main:\n"
+    "\tlet: i 0\n"
+    "\tlet: continue True\n"
+    "\tloop: continue\n"
+    "\t\tadd: i 1\n"
+    "\t\tset: i $$\n"
+    "\t\tinf: i 10\n"
+    "\t\tset: continue $$";
 
-    auto lex = lexer<std::string>(bin);
+  auto lex = lexer<std::string>(bin);
 
-    auto commands = parse(lex);
-    for (const auto &com : commands)
-    {
-        std::cout<<com.name<<std::endl;
-    }
+  auto commands = parse(lex);
+  for (const auto &com : commands)
+  {
+    std::cout<<com.name<<std::endl;
+  }
+
+  std::string binerror =
+    "main++:\n"
+    "\tlet: i 0\n"
+    "\tlet: continue true\n"
+    "\tloop: continue\n"
+    "\t\tadd: i 1\n"
+    "\t\tset: i $$\n"
+    "\t\tinf: i 10\n"
+    "\t\tset: continue $$";
+
+  auto &&tks = lexer(binerror).tokens();
+  auto iterror = std::count_if(std::begin(tks), std::end(tks),
+                               [] (const auto &tk)
+                               { return tk.type==bml::unknown; });
+  for (auto &&tk:tks)
+  {
+    std::cout<<(int) tk.type<<' '<<tk.line<<'\n';
+  }
+
+  std::cout<<iterror;
 }
