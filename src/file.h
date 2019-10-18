1dc46af2a3abf7fb28768a81301b3f3ab64b3c95
padget <padget.pro@gmail.com>
2019-10-18 08:23:51 +0400

    Update file.h

--- src/file.h
+++ src/file.h
@@ -15,10 +15,36 @@ enum file_error {
   FILE_BUFFER_ALLOCATION_ERROR
 };
 
-enum file_error blocks_fsize(FILE *file, size_t *size) ;
-enum file_error blocks_fclose(FILE *file);
-enum file_error blocks_fopen(const char *filename, FILE **file);
-enum file_error blocks_freadall(FILE *file, blocks_srange *range);
-enum file_error blocks_build_buffer(FILE *file, blocks_srange *range);
+typedef struct {
+union {
+file_error error;
+FILE* file;
+}
+int tag;
+} io_fopen_result;
+
+typedef struct {
+union {
+file_error error;
+size_t size;
+}
+int tag;
+} io_fsize_result;
+
+
+typedef struct {
+union {
+file_error error;
+blocks_srange data;
+}
+int tag;
+} io_freadall_result;
+
+
+void blocks_fsize(FILE *file, io_fsize_result* res) ;
+void blocks_fclose(FILE *file, io_fclose_result* res);
+void blocks_fopen(const char *filename, io_fopen_result* res);
+void blocks_freadall(FILE *file, io_freadall_result* res);
+void blocks_fbuffer(FILE *file, io_fbuffer_result* res );
 
 #endif
