94fd3c7b19c0526369219f959203f7d41ebe94be
benjamin <benjamin-externe.mathieu@enedis.fr>
2019-11-06 11:20:46 +0100

    add command from token

--- compile.sh
+++ compile.sh
@@ -1,12 +1,4 @@
-g++ -c src2/main.cpp -std=c++17
-g++ -c src2/command.cpp -std=c++17
-g++ -c src2/scanner.cpp -std=c++17
-g++ -c src2/token.cpp -std=c++17
-g++ -c src2/tokenizer.cpp -std=c++17
-
-
-g++ -o blocks.exe main.o \
-                command.o \
-                scanner.o \
-                token.o \
-                tokenizer.o
+echo compilation main.cpp
+g++ -c src2/main.cpp -std=c++17 -Wall -pedantic
+echo construction blocks.exe
+g++ -o blocks.exe main.o
--- src2/main.cpp
+++ src2/main.cpp
@@ -9,7 +9,14 @@ struct command_token
 
 struct basic_token
 {
+  blocks::string type;
   blocks::string value;
+};
+
+struct command
+{
+  blocks::string name;
+  blocks::vector<blocks::string> args;
 };
 
 blocks::vector<command_token>
@@ -20,14 +27,20 @@ tokenizes_command_to_tokens(const blocks::command_token& cmd);
 
 blocks::vector2d<basic_token>
 tokenizes_commands_to_tokens(const blocks::vector<blocks::command_token>& cmds);
+
+blocks::vector<blocks::command>
+build_commands_from_tokens(const blocks::vector2d<basic_token>& cmdstok);
+
+blocks::command
+build_command_from_tokens(const blocks::vector<basic_token>& cmdtok);
 }
 
 int
 main(int argc, char const* argv[])
 {
-  auto&& cmds = blocks::tokenizes_source_to_commands("");
-  auto&& tks2d = blocks::tokenizes_commands_to_tokens(cmds);
-
+  auto&& cmds = blocks::tokenizes_source_to_commands("main:\n add: 1 2");
+  auto&& cmds2d = blocks::tokenizes_commands_to_tokens(cmds);
+  auto&& cmds = blocks::build_commands_from_tokens(cmds2d);
   return 0;
 }
 
@@ -35,9 +48,8 @@ blocks::vector<blocks::command_token>
 blocks::tokenizes_source_to_commands(const blocks::string& source)
 {
   blocks::vector<command_token> cmds;
-
   blocks::string::const_iterator begin = source.begin();
-  blocks::string::const_iterator end = source.begin();
+  blocks::string::const_iterator end = source.end();
   blocks::string::const_iterator cursor = begin;
 
   while (cursor != end) {
@@ -45,34 +57,50 @@ blocks::tokenizes_source_to_commands(const blocks::string& source)
       cmds.push_back(command_token{ blocks::string{ begin, cursor } });
       begin = cursor + 1;
     }
-
     cursor++;
   }
+  cmds.push_back(command_token{ blocks::string{ begin, cursor } });
 
   return cmds;
 }
 
 namespace blocks {
-blocks::optional<basic_token>
+blocks::optional<blocks::basic_token>
 try_tokenize_name(blocks::string::const_iterator begin,
                   blocks::string::const_iterator end);
 
-blocks::optional<basic_token>
+blocks::optional<blocks::basic_token>
+try_tokenize_integer(blocks::string::const_iterator begin,
+                     blocks::string::const_iterator end);
+
+blocks::optional<blocks::basic_token>
 try_tokenize_colon(blocks::string::const_iterator begin,
                    blocks::string::const_iterator end);
 
-blocks::optional<basic_token>
+blocks::optional<blocks::basic_token>
 try_tokenize_space(blocks::string::const_iterator begin,
                    blocks::string::const_iterator end);
 
-blocks::optional<basic_token>
+blocks::optional<blocks::basic_token>
 try_tokenize_eol(blocks::string::const_iterator begin,
                  blocks::string::const_iterator end);
 
-blocks::optional<basic_token>
+blocks::optional<blocks::basic_token>
 try_tokenize_eos(blocks::string::const_iterator begin,
                  blocks::string::const_iterator end);
 } // namespace blocks
+
+blocks::vector2d<blocks::basic_token>
+blocks::tokenizes_commands_to_tokens(
+  const blocks::vector<blocks::command_token>& cmdstok)
+{
+  blocks::vector2d<blocks::basic_token> cmds;
+
+  for (auto&& cmdtok : cmdstok)
+    cmds.push_back(blocks::tokenizes_command_to_tokens(cmdtok));
+
+  return cmds;
+}
 
 blocks::vector<blocks::basic_token>
 blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
@@ -84,7 +112,7 @@ blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
 
   while (cursor != end) {
     {
-      auto&& eos = try_tokenize_space(cursor, end);
+      auto&& eos = blocks::try_tokenize_eos(cursor, end);
       if (eos.has_value()) {
         tokens.push_back(eos.value());
         cursor = std::next(cursor, eos.value().value.size());
@@ -92,7 +120,7 @@ blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
       }
     }
     {
-      auto&& space = try_tokenize_space(cursor, end);
+      auto&& space = blocks::try_tokenize_space(cursor, end);
       if (space.has_value()) {
         tokens.push_back(space.value());
         cursor = std::next(cursor, space.value().value.size());
@@ -100,7 +128,7 @@ blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
       }
     }
     {
-      auto&& name = try_tokenize_name(cursor, end);
+      auto&& name = blocks::try_tokenize_name(cursor, end);
       if (name.has_value()) {
         tokens.push_back(name.value());
         cursor = std::next(cursor, name.value().value.size());
@@ -108,7 +136,15 @@ blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
       }
     }
     {
-      auto&& colon = try_tokenize_colon(cursor, end);
+      auto&& integer = blocks::try_tokenize_integer(cursor, end);
+      if (integer.has_value()) {
+        tokens.push_back(integer.value());
+        cursor = std::next(cursor, integer.value().value.size());
+        continue;
+      }
+    }
+    {
+      auto&& colon = blocks::try_tokenize_colon(cursor, end);
       if (colon.has_value()) {
         tokens.push_back(colon.value());
         cursor = std::next(cursor, colon.value().value.size());
@@ -116,14 +152,90 @@ blocks::tokenizes_command_to_tokens(const blocks::command_token& cmd)
       }
     }
     {
-      auto&& eol = try_tokenize_eol(cursor, end);
+      auto&& eol = blocks::try_tokenize_eol(cursor, end);
       if (eol.has_value()) {
         tokens.push_back(eol.value());
         cursor = std::next(cursor, eol.value().value.size());
         continue;
       }
     }
+    // error case
+    {
+      tokens.push_back(
+        blocks::basic_token{ "error", blocks::string(cursor, cursor + 1) });
+      cursor++;
+    }
   }
 
   return tokens;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_name(blocks::string::const_iterator begin,
+                          blocks::string::const_iterator end)
+{
+  blocks::string::const_iterator cursor = begin;
+
+  while (cursor != end && 'a' <= *cursor && *cursor <= 'z')
+    cursor++;
+
+  if (cursor != begin)
+    return blocks::basic_token{ "name", blocks::string{ begin, cursor } };
+  else
+    return std::nullopt;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_integer(blocks::string::const_iterator begin,
+                             blocks::string::const_iterator end)
+{
+  blocks::string::const_iterator cursor = begin;
+
+  while (cursor != end && '0' <= *cursor && *cursor <= '9')
+    cursor++;
+
+  if (cursor != begin)
+    return blocks::basic_token{ "integer", blocks::string{ begin, cursor } };
+  else
+    return std::nullopt;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_colon(blocks::string::const_iterator begin,
+                           blocks::string::const_iterator end)
+{
+  if (begin != end && *begin == ':')
+    return blocks::basic_token{ "colon", blocks::string{ begin, begin + 1 } };
+  else
+    return std::nullopt;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_space(blocks::string::const_iterator begin,
+                           blocks::string::const_iterator end)
+{
+  if (begin != end && *begin == ' ')
+    return blocks::basic_token{ "space", blocks::string{ begin, begin + 1 } };
+  else
+    return std::nullopt;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_eol(blocks::string::const_iterator begin,
+                         blocks::string::const_iterator end)
+{
+  if (begin != end && *begin == '\n')
+    return blocks::basic_token{ "eol", blocks::string{ begin, begin + 1 } };
+  else
+    return std::nullopt;
+}
+
+blocks::optional<blocks::basic_token>
+blocks::try_tokenize_eos(blocks::string::const_iterator begin,
+                         blocks::string::const_iterator end)
+{
+  if (begin == end)
+    return blocks::basic_token{ "eos", "" };
+  else
+    return std::nullopt;
 }
