61d990aa6a49cafea29fde017fd27a9eef98711c
padget <padget.pro@gmail.com>
2020-01-08 08:05:44 +0100

    read all file as string

--- blc/src/main.cpp
+++ blc/src/main.cpp
@@ -1,17 +1,46 @@
+#include <fstream>
 #include <iostream>
+#include <iterator>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "../../include/cxx.hpp"
-#include "arguments.hpp"
+#include "../../include/arguments.hpp"
 
 
 int main(int argc, char** argv)
 {
-  auto&& args=cxx::from_args(argc, argv);
+  std::cout << "blocks-ccompile" << std::endl;
+  auto&& args = cxx::from_args(argc, argv);
+
   for (auto && arg: args) 
     std::cout << arg.first << " " << arg.second.value << std::endl;
-	std::cout << "coucou " << std::endl;
-	for (int i=0; i<argc; ++i)
-		std::cout << argv[i] << "\n";
-	return 0;
- }
+  // une fois quon a lesenble des arguments, on doit verifier et prendre 
+  // largument qui mindique quel fichier blc doit compiler et transformer 
+  // en bytecode. le bytecode pourra ensuite etre serialiser dans un fichier
+  // <name>.blby
+
+
+  // ici on va s'assurer que l'argument <file> est bien présent dans la ligne
+  // da commande. Une fois que l'argument est bien détecté, on va pouvoir lire
+  // le fichier et le transformer en std::string et lancer le processus de
+  // transoformation en un ensemble de commandes et ensuite en l'écriture d'un
+  // fichier bytecode simple uniquement avec les ordres des commandes.
+  // Nous verrons plus tard pour ajouter les arguments et la construction du
+  // de la mémoire des variables et des constantes.
+
+
+  if (args.count("--file") == 1)
+  {
+    auto filename = args.at("--file").value;
+    std::cout << "fichier à compiler " << filename << std::endl; 
+    std::ifstream is(args.at("--file").value);
+    std::istreambuf_iterator<char> endfile;
+    std::istreambuf_iterator<char> beginfile(is);
+    auto src = std::string(beginfile, endfile);
+
+    std::cout << src << std::endl;
+  }
+
+  return 0;
+}
--- include/arguments.hpp
+++ include/arguments.hpp
@@ -5,54 +5,65 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 #define let(name, type, value) #type #name=#value
 
 namespace cxx
 {
-	struct argument 
-	{
-		std::string name;
-		std::string value;
-//		TODO pour l'instant nous ne g√rerons pas les 
-//		description dans le module de gestion des 
-//		arguments du programme. Donc pas de page help.
-//		std::string description;
-	};
+struct argument 
+{
+  std::string name;
+  std::string value;
+  //		TODO pour l'instant nous ne g√rerons pas les 
+  //		description dans le module de gestion des 
+  //		arguments du programme. Donc pas de page help.
+  //		std::string description;
+};
 
-	std::pair<std::string, std::string> 
-		split_eq(const std::string& str)
-	{
-		auto begin = str.begin();
-		auto end = str.end();
-	  auto iseq = [](auto c){return c=='=';};
-		auto equal = std::find_if(begin, end, iseq);
-		auto value = std::string(str.begin(), equal);
-		auto arg =  std::string{equal+1, str.end()};
-		return {arg, value};
-	}
+std::pair<std::string, std::string> 
+split_eq(const std::string& str)
+{
+  auto begin = str.begin();
+  auto end = str.end();
+  auto iseq = [](auto c){return c=='=';};
+  auto equal = std::find_if(begin, end, iseq);
+  auto value = std::string(str.begin(), equal);
+  auto arg =  std::string{equal+1, str.end()};
+  return {value, arg};
+}
 
-	std::map<std::string, argument> 
-		from_args(int argc, char** argv)
-	{
-		std::map<std::string, argument> args;;
-		std::vector<std::string> sargs;
-		int i = argc-1;
-		
-		while (i>=2) {
-			sargs.push_back(argv[i]);
+std::map<std::string, argument> 
+from_args(int argc, char** argv)
+{
+  std::cout << "from args" << std::endl;
+  std::map<std::string, argument> args;;
+  std::vector<std::string> sargs;
+  int i = argc-1;
+
+  std::cout << argc << std::endl;
+  
+  while (i>=1) 
+  {
+    sargs.push_back(argv[i]);
     i--;
-    }
-		for (std::string& arg:sargs)
-		{
-      auto&& pair = split_eq(arg);
-      auto&& name = pair.first;
-      auto&& value = pair.second;
-			args.insert({name, {name, value}});
-		}
+  }
+  
+  std::cout << "coucou1" << std::endl;
 
-		return args;
-	}
+  for (std::string& arg:sargs)
+  {
+    std::cout << arg << std::endl;
+    auto&& pair = split_eq(arg);
+    auto&& name = pair.first;
+    auto&& value = pair.second;
+    args.insert({name, {name, value}});
+  }
+
+  std::cout << "coucou 2" << std::endl;
+
+  return args;
+}
 }
 
 #endif
