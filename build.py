45195ca8a6042261b43cccc042315067cb456352
padget <padget.pro@gmail.com>
2020-03-01 12:28:23 +0000

    add build.py

---
+++ build.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def build_help():
+    print('blocks-help : building...')
+
+    print('blokcs-help : built success !')
+
+
+if __name__=='__main__':
+    print('building project')
+    build_help()
+
+
