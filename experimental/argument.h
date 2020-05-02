33790224de3d3ffb862abffab8cb0b01ebfc4c4c
padget <padget.pro@gmail.com>
2020-05-02 10:11:30 +0100

    ajout register arg rule

--- experimental/argument.h
+++ experimental/argument.h
@@ -12,6 +12,14 @@ struct syscall
 
 typedef struct syscall syscall;
 
+struct sysargrule
+{
+	bool mandatory;
+	const char* description;
+};
+
+typedef struct sysargrule sysargrule;
+
 // an instance of syscall to have
 // access to syscall args everywhere
 // in the source code.
@@ -19,6 +27,8 @@ extern syscall sys;
 
 
 void register_args(int argc, char** argv);
+// TODO implement
+void register_arg_rule(const char* name, sysargrule);
 
 bool args_exists(const char* name);
 char** args_value(const char* name);
